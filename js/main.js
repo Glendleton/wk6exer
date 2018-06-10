a1394aca52a9900aa794e52a78f94af21fad22d4
Glendleton <gpendlet92@gmail.com>
2018-06-09 17:00:15 -0700

    finish from step 5 onward

---
+++ js/main.js
@@ -0,0 +1,88 @@
+// step 5)
+var testOne = "What kind of variable am I?"
+
+var testTwo = 256;
+
+var testThree = false;
+
+var testFour = 'true';
+
+var testFive = '128';
+
+var testSix = "";
+
+// step 6)
+var testOne = "hello" + "there";
+// they will concatenate
+
+var testTwo= '100' + 28;
+// 10028
+
+var testThree = true + 3;
+// 4
+
+var testFour = 'false' + 5;
+// false5
+
+var testFive = '128' - 3;
+
+var testSix = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// step 7)
+// we will get a sentence of concatenated variables
+
+// step 8)
+// they are added up to 120
+
+// step 9)
+// 10ten10
+
+// step 10)
+// num is greater than 1
+
+// step 11)
+// else if statement ran
+
+// step 12)
+// else if statement ran
+
+// step 13)
+// 0 thru 16 with each character of the string
+
+// step 14)
+// 5
+
+// step 15)
+// it will log each o
+
+// step 16)
+// it will log all the vowels
+
+// step 17)
+// it will run each argument according to the parameters
+
+// step 18)
+// it will tell you if the number is equal to or greater or not
+
+// step 19)
+// it will tell you how many vowels are there
